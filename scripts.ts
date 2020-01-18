ceeceb59caa32319f2c90023b3a5bdeb05cf611b
Bakidjan <55193201+bakidjan@users.noreply.github.com>
2020-01-18 09:05:19 +0100

    Update scripts.ts

--- scripts.ts
+++ scripts.ts
@@ -1,3 +1,5 @@
+#An unhandled exception occurred: Invalid or unexpected token
+#See "C:\Users\Bakidjan\AppData\Local\Temp\ng-FFMZEl\angular-errors.log" for further details.
 rmdir /s node_modules
 rmdir /s dist
 npm install --save-dev @angular/cli@latest
