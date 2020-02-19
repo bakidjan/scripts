a27726037bc6d20962d14d613c54b8fc26217bc6
Bakidjan <55193201+bakidjan@users.noreply.github.com>
2020-02-19 16:13:53 +0100

    Update scripts.ts

--- scripts.ts
+++ scripts.ts
@@ -6,3 +6,24 @@ rmdir /s dist
 npm install --save-dev @angular/cli@latest
 npm install
 ng update
+
+
+# Comment effacer .m2/repository
+1. C:\Program Files\Apache Software Foundation\apache-maven-3.6.3\conf
+modifier le fichier "settings.xml"
+modifier le :
+   | Default: ${user.home}/.m2/repository
+   |<localRepository>/path/to/local/repo</localRepository>
+
+    
+   en 
+
+"C:/Program Files/.m2/repository" par exemple, 
+  
+  ouvert un terminal en superUser 
+  créer un folder
+  se placer dedans et executer "mvn archetype:generate"
+  donner archetyId : com.diallo par exemple
+  donner le package
+  select with proposed number for mvn archetype : 7 for exemple to mwv-archetype-webapp
+  supprimer le default "/.m2/repository" et hope
